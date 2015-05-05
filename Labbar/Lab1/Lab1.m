b125c630f725f86531b450081d68b09dcd430ca3
unknown <danbo324@itn-tp5021-03.ad.liu.se>
2015-05-05 10:00:36 +0200

    About to start assignm. 15

--- Labbar/Lab1/Lab1.m
+++ Labbar/Lab1/Lab1.m
@@ -76,4 +76,52 @@ y = conv(x, h);
 soundsc(y, fs);
 
 %% 7
+clear all; clc;
+
+% sjukt star wars
+load('piano1.mat')
+fs = fs*0.5;
+soundsc(y1,fs);
+pause(0.5)
+soundsc(y1,fs);
+pause(0.5)
+soundsc(y1,fs);
+pause(0.5)
+soundsc(y1,fs*0.85);
+pause(0.25)
+soundsc(y1,fs*1.15);
+pause(0.3)
+soundsc(y1,fs);
+pause(0.5)
+soundsc(y1,fs*0.85);
+pause(0.25)
+soundsc(y1,fs*1.15);
+pause(0.3)
+soundsc(y1,fs);
+pause(1)
+soundsc(y1,fs*1.55);
+pause(0.5)
+soundsc(y1,fs*1.55);
+pause(0.5)
+soundsc(y1,fs*1.55);
+pause(0.5)
+
+%% 8a
+clear all; clc;
+load('piano1.mat')
+
+
+X = fft(y1);
+N = length(X);
+f = 0:fs/N:fs/2;
+X_magn=abs(X)/(N/2);
+
+plot(f,X_magn(1:length(f)));
+grid;
+
+
+%% 15
+clear all; clc;
+load('phonenumber.mat')
+
 
